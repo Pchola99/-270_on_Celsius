9a49b6f30f27d685287234ef96c6a4d6dc9cf93d
Pchola99 <oulalaa6@gmail.com>
2022-12-13 23:21:44 +0300

    *

--- src/core/World/WorldGenerator.java
+++ src/core/World/WorldGenerator.java
@@ -6,7 +6,7 @@ public class WorldGenerator {
 
         for (int x = 0; x < SizeX; x++) {
             for (int y = 0; y < SizeY; y++) {
-                if (x < SizeX / 2) {
+                if (y < SizeX / 2) {
                     WorldObjects grass = new WorldObjects(false, true, false, false, true, false, false, false, false, null, "D:\\-270_on_Celsius\\src\\assets\\World\\grass1.png", x * 16, y * 32);
                     StaticObjects[x][y] = grass;
                 } else {
