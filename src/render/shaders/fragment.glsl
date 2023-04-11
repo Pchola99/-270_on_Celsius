154a8ab2bff5814e56d679cf93dc58d156921b0b
Pchola99 <oulalaa6@gmail.com>
2023-04-11 21:04:00 +0300

    *

--- src/render/shaders/fragment.glsl
+++ src/render/shaders/fragment.glsl
@@ -1,10 +1,10 @@
-#version 330 core
+#version 450 core
 
 in vec2 texCoord;
 out vec4 fragColor;
 
-uniform sampler2D myTexture;
+uniform sampler2D tex;
 
 void main() {
-    fragColor = texture(myTexture, texCoord);
+    fragColor = texture(tex, texCoord);
 }
