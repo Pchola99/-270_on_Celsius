4d530d71d7e8809a0d5c37e744c6162ba04ef6fa
Pchola99 <oulalaa6@gmail.com>
2023-04-15 22:29:08 +0300

    Multichannel output! Multiple sounds can be played simultaneously in one unit of tim

--- src/core/World/Sound.java
+++ src/core/World/Sound.java
@@ -1,64 +1,57 @@
 package core.World;
 
-import core.Physics;
-
 import javax.sound.sampled.*;
 import java.io.File;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class Sound extends Thread {
-    private static String path;
-    private static boolean stop = true;
-    private static ConcurrentHashMap<String, File> sounds = new ConcurrentHashMap<>();
+public class Sound {
+    private static ConcurrentHashMap<String, Boolean> sounds = new ConcurrentHashMap<>();
 
     public static void SoundPlay(String path) {
-        Sound.path = path;
-        Sound.stop = false;
-
-        if (sounds.isEmpty()) new Thread(new Sound()).start();
-        sounds.putIfAbsent(path, new File(path));
-    }
-
-    public void run() {
-        while (true) {
-            if (!stop) {
-                try {
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(sounds.get(path));
-                    AudioFormat format = inputStream.getFormat();
-
-                    // преобразовать формат в PCM
-                    if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
-                        format = new AudioFormat(
-                                AudioFormat.Encoding.PCM_SIGNED,
-                                format.getSampleRate(),
-                                format.getSampleSizeInBits() * 2,
-                                format.getChannels(),
-                                format.getFrameSize() * 2,
-                                format.getFrameRate(),
-                                true);
-                        inputStream = AudioSystem.getAudioInputStream(format, inputStream);
-                    }
-
-                    // создать source data line и воспроизвести звук
-                    DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-                    SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
-                    sourceDataLine.open();
-                    sourceDataLine.start();
-                    byte[] bytesBuffer = new byte[4096];
-                    int bytesRead = -1;
-                    while ((bytesRead = inputStream.read(bytesBuffer)) != -1) {
-                        sourceDataLine.write(bytesBuffer, 0, bytesRead);
-                    }
-                    sourceDataLine.drain();
-                    sourceDataLine.stop();
-                    sourceDataLine.close();
-                    stop = true;
-
-                } catch (Exception e) {
-                    System.err.println("Error during sound playback: " + e);
-                }
-            }
-            Sound.yield();
+        if (sounds.get(path) != null && sounds.get(path)) {
+            return;
         }
+
+        new Thread(() -> {
+            try {
+                sounds.put(path, true);
+
+                AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
+                AudioFormat format = inputStream.getFormat();
+
+                // преобразовать формат в PCM
+                if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
+                    format = new AudioFormat(
+                            AudioFormat.Encoding.PCM_SIGNED,
+                            format.getSampleRate(),
+                            format.getSampleSizeInBits() * 2,
+                            format.getChannels(),
+                            format.getFrameSize() * 2,
+                            format.getFrameRate(),
+                            true);
+                    inputStream = AudioSystem.getAudioInputStream(format, inputStream);
+                }
+
+                DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+                SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
+                sourceDataLine.open(format);
+                sourceDataLine.start();
+
+                byte[] bytesBuffer = new byte[4096];
+                int bytesRead = -1;
+
+                while ((bytesRead = inputStream.read(bytesBuffer)) != -1) {
+                    sourceDataLine.write(bytesBuffer, 0, bytesRead);
+                }
+                sourceDataLine.drain();
+                sourceDataLine.stop();
+                sourceDataLine.close();
+
+            } catch (Exception e) {
+                System.err.println("Error during sound playback: " + e);
+            } finally {
+                sounds.put(path, false);
+            }
+        }).start();
     }
 }
